9dc2ddf8abcd645728b589f1ca1e6a00646feb89
Yuri <yurifonseca.comercial@gmail.com>
2025-03-26 08:13:12 -0300

    up

--- exercicios/seletores/script.js
+++ exercicios/seletores/script.js
@@ -8,6 +8,24 @@ Adicionar uma classe a todos os parágrafos.
 
 Alterar o texto do botão. */
 
-document.querySelectorAll('h1').forEach(function(element) {
-    element.innerText = 'Novo Texto para o H1';
-});
+let elemento = document.getElementById("title")
+elemento.innerText = "Movimento de Translação dos Planetas e a Regra de Kepler"
+function alterarlista() { 
+    let mudarlista = document.querySelectorAll("li")
+    mudarlista.forEach(li => { 
+        li.style.color = "red"
+        li.style.fontSize = "14px"
+        li.style.backgroundColor = "lightgray"
+    })
+    }
+function adicionarclasse() { 
+    let paragrafos = document.querySelectorAll("p")
+    paragrafos.forEach(p => p.classList.add("destacado"))
+}
+function alterarbotao() {
+    let botao = document.getElementById("botao")
+    botao.innerText = botao.innerText === "Clique aqui" ? "Texto alterado" : "Clique aqui"
+}
+alterarlista()
+adicionarclasse()
+alterarbotao()
