d3df026c863b6f03ac0b22cf5c07871e98e0f1eb
Yuri <yurifonseca.comercial@gmail.com>
2025-03-28 10:27:29 -0300

    up

---
+++ exercicios/dados2/script.js
@@ -0,0 +1,20 @@
+/* Altere a questão 2 do exercício anterior (Eventos) para armazenar no localStorage a lista de pessoas que clicaram no botão Curtir para que elas persistam ao recarregar a página.
+
+Você também deve incluir um botão "Limpar" que remove todos os nomes do localStorage.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Relembrando o enunciado
+
+Construa uma página HTML com um campo de texto, um botão "Curtir" e um parágrafo que exibe a lista de pessoas que clicaram no botão curtir. Toda vez que se preencher um nome no campo de texto e clicar em curtir, seu código Javascript deve armazenar o nome em um array (verifique se o nome já está lá) e alterar o parágrafo final da página de acordo com as regras:
+
+Lista vazia: "Ninguém curtiu"
+
+1 pessoa curtiu: "[Nome da pessoa] curtiu"
+
+2 pessoas curtiram: "[Pessoa 1] e [Pessoa 2] curtiram"
+
+3 ou mais pessoas curtiram:
+
+"[Pessoa 1], [Pessoa 2] e mais [tamanho da lista - 2] pessoas curtiram" */
+
